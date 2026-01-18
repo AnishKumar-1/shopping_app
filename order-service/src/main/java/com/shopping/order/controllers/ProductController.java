16a72dabb1c33a90de130b181e5164d0281640d5
anish <kumaranish0815@gmail.com>
2026-01-18 14:24:01 +0530

    fetch-order-details by order id

--- order-service/src/main/java/com/shopping/order/controllers/ProductController.java
+++ order-service/src/main/java/com/shopping/order/controllers/ProductController.java
@@ -2,15 +2,13 @@ package com.shopping.order.controllers;
 
 import com.shopping.order.dto.orderDto.OrderCreationResponseDto;
 import com.shopping.order.dto.orderDto.OrderRequestDto;
+import com.shopping.order.dto.orderDto.OrderResponseDto;
 import com.shopping.order.services.OrderService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -24,5 +22,11 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<OrderCreationResponseDto> createOrder(@Valid @RequestBody OrderRequestDto request){
         return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(request));
+    }
+
+    //get order details by its id
+    @GetMapping("/{order_id}")
+    public ResponseEntity<OrderResponseDto> SingleOrderDetails(@PathVariable Long order_id){
+        return ResponseEntity.status(HttpStatus.OK).body(orderService.fetch_single_order(order_id));
     }
 }
