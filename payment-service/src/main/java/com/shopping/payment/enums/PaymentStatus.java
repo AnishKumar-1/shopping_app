9c9fc46e6bb83fca08f20d0b758c70c07d92d754
anish <kumaranish0815@gmail.com>
2026-02-01 10:45:41 +0530

    payment service initial setup

---
+++ payment-service/src/main/java/com/shopping/payment/enums/PaymentStatus.java
@@ -0,0 +1,8 @@
+package com.shopping.payment.enums;
+
+public enum PaymentStatus {
+    SUCCESS,
+    FAILED,
+    CANCELLED,
+    PENDING
+}
---
+++ payment-service/src/main/java/com/shopping/payment/exception/GlobalException.java
@@ -0,0 +1,33 @@
+package com.shopping.payment.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    ErrorResponse error;
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpServletRequest request){
+        List<String> argError= exception.getBindingResult().getFieldErrors()
+                .stream().map(error->error.getField() + ": " + error.getDefaultMessage()).toList();
+        error= ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error("Validation Failed")
+                .message("Invalid request data")
+                .errorCode("VALIDATION_ERROR")
+                .details(argError)
+                .path(request.getRequestURL().toString())
+                .timestamp(LocalDateTime.now()).build();
+        return ResponseEntity.badRequest().body(error);
+    }
+
+}
